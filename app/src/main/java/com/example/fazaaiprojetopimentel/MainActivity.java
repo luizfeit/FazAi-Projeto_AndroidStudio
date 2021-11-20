54ebe209eedcebf44f0ca4c5577e8af416cbc0fa
luizfeit <luiz.felipe301@hotmail.com>
2021-11-20 11:02:35 -0300

    Cadastro Usuario Funcionando

--- app/src/main/java/com/example/fazaaiprojetopimentel/MainActivity.java
+++ app/src/main/java/com/example/fazaaiprojetopimentel/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.fazaaiprojetopimentel;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.database. sqlite.SQLiteDatabase;
 import android.view.View;
 import android.widget.*;
-
 public class MainActivity extends Activity {
 
     Button btnCadastrar;
 
+    SQLiteDatabase db;
 
 
     @Override
@@ -19,12 +22,31 @@ public class MainActivity extends Activity {
 
         btnCadastrar = (Button) findViewById(R.id.btncadastrar);
 
+
+        try{
+            db= openOrCreateDatabase("faz_assim", Context.MODE_PRIVATE, null);
+            db.execSQL("create table if not exists " +
+                    "usuarios(id integer primary key " +
+                    "autoincrement, nome text not null, email text " +
+                    "not null, senha text not null, funcao integer not null)");
+
+        }catch (Exception e){
+            MostraMessagem("Erro ao criar o Banco de Dados: " + e.toString());
+        }
+
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cadastrarDados = new Intent(MainActivity.this, CadastrarAtividadeActivity.class);
+                Intent cadastrarDados = new Intent(MainActivity.this, TelaDeCadastroActivity.class);
                 MainActivity.this.startActivity(cadastrarDados);
             }
         });
+    }
+    public void MostraMessagem(String str){
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
+        dialogo.setTitle("Aviso");
+        dialogo.setMessage(str);
+        dialogo.setNeutralButton("Ok", null);
+        dialogo.show();
     }
 }
--- app/src/main/java/com/example/fazaaiprojetopimentel/TelaDeCadastroActivity.java
+++ app/src/main/java/com/example/fazaaiprojetopimentel/TelaDeCadastroActivity.java
@@ -1,14 +1,88 @@
 package com.example.fazaaiprojetopimentel;
 
 import android.app.Activity;
-
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.database. sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.*;
 
 public class TelaDeCadastroActivity extends Activity {
+
+    Button btCadastrar, btCancelar;
+    EditText txNome, txEmail,txSenha;
+    RadioButton rdFuncionario, rdAdministrador ;
+    SQLiteDatabase db;
+
+    Integer funcao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_de_cadastro);
+
+        btCadastrar = (Button) findViewById(R.id.btncadastrar);
+        btCancelar = (Button) findViewById(R.id.btncancelar);
+
+        txNome = (EditText) findViewById(R.id.txnome);
+        txEmail = (EditText) findViewById(R.id.txemail);
+        txSenha = (EditText) findViewById(R.id.txsenha);
+
+        rdFuncionario = (RadioButton) findViewById(R.id.rdfuncionario);
+        rdAdministrador = (RadioButton) findViewById(R.id.rdadministrador);
+
+        try{
+            db = openOrCreateDatabase("faz_assim", Context.MODE_PRIVATE, null);
+        }catch (Exception e){
+            MostraMessagem ("Erro: " + e.toString());
+        }
+
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = txNome.getText().toString();
+                String email = txEmail.getText().toString();
+                String senha = txSenha.getText().toString();
+
+
+                if(rdFuncionario.isChecked()){
+                     funcao = 1;
+                }else{
+                     funcao = 2;
+                }
+
+                ContentValues valor = new ContentValues();
+
+                valor.put("nome", nome);
+                valor.put("email", email);
+                valor.put("senha", senha);
+                valor.put("funcao", funcao);
+
+                System.out.println(nome + " " + email + " " + senha+ " " + funcao );
+
+
+
+                try{
+                    db.insert("usuarios", null, valor);
+                    MostraMessagem("Registrado com Sucesso");
+                }catch (Exception e){
+                    //MostraMessagem("Algo deu errado ao Cadastrar: " + e.printStackTrace());
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
     }
+    public void MostraMessagem(String str){
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(TelaDeCadastroActivity.this);
+        dialogo.setTitle("Aviso");
+        dialogo.setMessage(str);
+        dialogo.setNeutralButton("Ok", null);
+        dialogo.show();
+    }
+
 }
