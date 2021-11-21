a127074f4927b42bfb2aabb9107930361b466e2b
luizfeit <luiz.felipe301@hotmail.com>
2021-11-21 17:33:23 -0300

    Adicionar Fotos

--- app/src/main/java/com/example/fazaaiprojetopimentel/VIsualizarAtividadesActivity.java
+++ app/src/main/java/com/example/fazaaiprojetopimentel/VIsualizarAtividadesActivity.java
@@ -1,14 +1,43 @@
 package com.example.fazaaiprojetopimentel;
 
 import android.app.Activity;
-
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.database. sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.*;
 
 public class VIsualizarAtividadesActivity extends Activity {
+
+    ImageView imgAdicionar,imgSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizar_atividades);
+
+        imgAdicionar = (ImageView) findViewById(R.id.imgadicionar);
+
+
+        imgAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cadastrarAtividade = new Intent(VIsualizarAtividadesActivity.this,
+                        CadastrarAtividadeActivity.class);
+                VIsualizarAtividadesActivity.this.startActivity(cadastrarAtividade);
+            }
+        });
+
+        imgSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logout = new Intent(VIsualizarAtividadesActivity.this,
+                        TelaDeCadastroActivity.class);
+                VIsualizarAtividadesActivity.this.startActivity(logout);
+            }
+        });
     }
 }
